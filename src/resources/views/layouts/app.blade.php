c7cdd89677923f7926eb5803371632cf25091592
taro <asa.umareru.2@gmail.com>
2026-01-15 00:18:25 +0900

    認証コミット

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -15,9 +15,26 @@
 <body>
   <header class="header">
     <div class="header__inner">
+      <div class="header-utilities">
       <a class="header__logo" href="/">
         Contact Form
       </a>
+       <nav>
+          <ul class="header-nav">
+            @if(Auth::check())
+            <li class="header-nav__item">
+              <a class="header-nav__link" href="/mypage">マイページ</a>
+            </li>
+            <li class="header-nav__item">
+              <form class="form" action="/logout" method="post">
+                @csrf
+                <button class="header-nav__button">ログアウト</button>
+              </form>
+            </li>
+            @endif
+          </ul>
+        </nav>
+      </div>
     </div>
   </header>
 
