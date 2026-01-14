2265b633d9ba2e195d66ac782a71ab4a660b57d5
taro <asa.umareru.2@gmail.com>
2026-01-14 14:13:46 +0900

    第三コミット

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
+
+Route::get('/',[ContactController::class,'index']);
+Route::post('/contacts/confirm',[ContactController::class,'confirm']);
+Route::post('/contacts',[ContactController::class,'store']);
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +18,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
